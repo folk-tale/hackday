d485a8b4d1c1f667a590c3fe0c8d789fa685bc8e
bhnascar <bhnascar@gmail.com>
2015-07-29 23:27:30 -0700

    Changing search method, prepending props instead of appending

--- app/scripts/search.js
+++ app/scripts/search.js
@@ -12,26 +12,6 @@ function addAvatarToBackstage() {
   document.getElementById("content").appendChild(propDiv);
 }
 
-// For old Image Search API (we actually use this, though)
-function hndlr(response) {
-  for (var i = response.responseData.results.length - 1; i >=0 ; i--) {
-    var item = response.responseData.results[i];
-    var id = "i"+Math.random();
-    var propDiv = makeDraggableElement(item.url, id);
-    document.getElementById("content").appendChild(propDiv);
-  }
-}
-
-// Using old Image Search API (allows searching for transparent images)
-function searchNoun(noun) {
-  var query = encodeURI(noun);
-  var reqURL = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&callback=hndlr&rsz=8&imgc=trans&imgsz=medium&safe=active&q=" + query;
-  var script = document.createElement("script");
-  script.src = reqURL;
-  document.getElementById("content").appendChild(script);
-  document.getElementById("terms").innerHTML += "<span class=\"term\">" + noun + "</span>";
-}
-
 function clearAllButAvatars() {
   $("#content").children('div').each(function(i) { 
     if (! $(this).hasClass('avatar')) {
@@ -40,9 +20,35 @@ function clearAllButAvatars() {
   });
 }
 
+// For old Image Search API (we actually use this, though)
+function hndlr(response) {
+  var contentarea = document.getElementById("content");
+  for (var i = response.responseData.results.length - 1; i >=0 ; i--) {
+    var item = response.responseData.results[i];
+    var id = "i"+Math.random();
+    var propDiv = makeDraggableElement(item.url, id);
+    contentarea.insertBefore(propDiv, contentarea.firstChild);
+  }
+}
+
+// Using old Image Search API (allows searching for transparent images)
+function addSearchButton(noun) {
+  var button = document.createElement("span");
+  button.textContent = noun;
+  button.style.cursor = "pointer";
+  button.classList.add("term");
+  button.addEventListener("click", function() {
+    var query = encodeURI(noun);
+    var reqURL = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&callback=hndlr&rsz=8&imgc=trans&imgsz=medium&safe=active&q=" + query;
+    var script = document.createElement("script");
+    script.src = reqURL;
+    document.getElementById("content").appendChild(script);
+  })
+  document.getElementById("terms").appendChild(button);
+}
+
 function searchAll() {
   clearAllButAvatars();
-  //document.getElementById("content").innerHTML = "";
   document.getElementById("terms").innerHTML = "";
   var input = document.querySelector("#queryfield").value;
   var sentence = nlp.pos(input).sentences[0];
@@ -58,13 +64,21 @@ function searchAll() {
     {
       continue;
     }
-    searchNoun(noun.text);
+    addSearchButton(noun.text);
   }
 }
 
-
-$("#queryfield").keypress(function(e) {
-  if (e.keyCode === 0 || e.keyCode === 32 || e.keyCode === 190 || e.keyCode === 188 || e.keyCode === 110  || e.keyCode === 46 || e.keyCode === 44) {
+// Auto search
+$("#queryfield").keyup(function(e) {
+  /*
+  if (e.keyCode === 13 ||     // Enter
+      e.keyCode === 32 ||     // Space
+      e.keyCode === 190 ||    // Period
+      e.keyCode === 188 ||    // Comma 
+      e.keyCode === 110  ||   // Decimal point
+      e.keyCode === 46 ||     // Delete
+      e.keyCode === 8) {      // Backspace
     searchAll();
-  }
+  }*/
+  searchAll(); // Lolz
 });
